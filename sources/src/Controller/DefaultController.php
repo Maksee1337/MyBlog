993d82bae3fd950ccf9bd10c98d87d832fcae574
Maksee1337 <maksee1337@gmail.com>
2021-08-02 18:42:47 +0300

    Lesson 5

---
+++ sources/src/Controller/DefaultController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\News;
+use App\Entity\Users;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class DefaultController
+ *
+ * @package App\Controller
+ *
+ * @author Maks Voytenko <m.voytenko1991@gmail.com>
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="Default_Index")
+     *
+     * @return Response
+     */
+    public function Index()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $news = $em->getRepository(News::class)->findAll();
+
+        return $this->render('Default/Index.html.twig', ['news' => $news]);
+    }
+
+    /**
+     * @Route("/About", name="Default_About")
+     *
+     * @return Response
+     */
+    public function About()
+    {
+        return $this->render('Default/About.html.twig', ['text' => '222some text']);
+    }
+
+    /**
+     * @Route("/Feedback", name="Default_Feedback")
+     *
+     * @return Response
+     */
+    public function Feedback()
+    {
+        return $this->render('Default/Feedback.html.twig');
+    }
+
+    /**
+     * @Route("/GenerateNews", name="Default_GenerateNews")
+     *
+     * @return Response
+     */
+    public function GenerateNews() :Response
+    {
+        $datetime = new \DateTime();
+        $hash = uniqid();
+        $news = new News();
+        $news->setAuthor('Author');
+        $news->setDateTime($datetime);
+        $news->setShort('Short Description '.$hash);
+        $news->setText('Full Description '. $hash);
+        $news->setViews(0);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($news);
+        $em->flush();
+
+        return $this->render('Default/GenerateNews.html.twig', [
+                                                                    'DateTime' => $datetime->format('d/m/Y H:i:s'),
+                                                                    'hash' => $hash
+                                                                    ]);
+    }
+}
---
+++ sources/src/Entity/News.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NewsRepository::class)
+ */
+class News
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=350)
+     */
+    private $Short;
+
+    /**
+     * @ORM\Column(type="string", length=20000)
+     */
+    private $Text;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Author;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $DateTime;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $Views;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getShort(): ?string
+    {
+        return $this->Short;
+    }
+
+    public function setShort(string $Short): self
+    {
+        $this->Short = $Short;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->Text;
+    }
+
+    public function setText(string $Text): self
+    {
+        $this->Text = $Text;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->Author;
+    }
+
+    public function setAuthor(?string $Author): self
+    {
+        $this->Author = $Author;
+
+        return $this;
+    }
+
+    public function getDateTime(): ?\DateTimeInterface
+    {
+        return $this->DateTime;
+    }
+
+    public function setDateTime(\DateTimeInterface $DateTime): self
+    {
+        $this->DateTime = $DateTime;
+
+        return $this;
+    }
+
+    public function getViews(): ?int
+    {
+        return $this->Views;
+    }
+
+    public function setViews(int $Views): self
+    {
+        $this->Views = $Views;
+
+        return $this;
+    }
+}
